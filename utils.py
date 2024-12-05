5bcb75db9e88d9082f564dabdda9c8d04606f4a9
ZixiZhaoxjy <53925770+ZixiZhaoxjy@users.noreply.github.com>
2024-12-05 14:13:24 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,7 +3,9 @@ import torch.nn as nn
 
 def mape_loss(y_pred, y_true):
     return torch.mean(torch.abs((y_true - y_pred) / y_true))
-
+    
+# MyNetwork1 is chemical process prediction model considering initial manufacturing variability (ChemicalProcessModel)
+# Input U + cycles; Output features
 class MyNetwork1(nn.Module):
     def __init__(self):
         super(MyNetwork1, self).__init__()
@@ -18,7 +20,8 @@ class MyNetwork1(nn.Module):
         x = torch.relu(self.fc3(x))
         x = self.fc4(x)
         return x
-
+        
+# MyNetwork2 is battery degradation trajectory model : Input features + U + cycles; Output capacity
 class MyNetwork2(nn.Module):
     def __init__(self):
         super(MyNetwork2, self).__init__()
@@ -34,17 +37,4 @@ class MyNetwork2(nn.Module):
         x = self.fc4(x)
         return x
 
-class MyNetwork3(nn.Module):
-    def __init__(self):
-        super(MyNetwork3, self).__init__()
-        self.fc1 = nn.Linear(11, 32)
-        self.fc2 = nn.Linear(32, 64)
-        self.fc3 = nn.Linear(64, 32)
-        self.fc4 = nn.Linear(32, 42)
 
-    def forward(self, x):
-        x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
-        x = torch.relu(self.fc3(x))
-        x = self.fc4(x)
-        return x
