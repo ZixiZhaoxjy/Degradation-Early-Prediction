7b6daf9413524fd3f28b4ae945e48b54093377fd
ZixiZhaoxjy <53925770+ZixiZhaoxjy@users.noreply.github.com>
2024-12-05 14:53:45 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -36,5 +36,20 @@ class MyNetwork2(nn.Module):
         x = torch.relu(self.fc3(x))
         x = self.fc4(x)
         return x
+        
+class MyNetwork3(nn.Module):
+    def __init__(self):
+        super(MyNetwork1, self).__init__()
+        self.fc1 = nn.Linear(11, 32)
+        self.fc2 = nn.Linear(32, 64)
+        self.fc3 = nn.Linear(64, 32)
+        self.fc4 = nn.Linear(32, 42)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = torch.relu(self.fc3(x))
+        x = self.fc4(x)
+        return x
 
 
